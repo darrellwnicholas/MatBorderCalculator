e3e7a090bb2d2e2750c31e5da963d63e22e5b6dd
Darrell Nicholas <darrellwnicholas@gmail.com>
2015-01-24 18:27:08 -0600

    Initial Commit

---
+++ MatBorder Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MatBorder Calculator
+//
+//  Created by Darrell Nicholas on 1/24/15.
+//  Copyright (c) 2015 Darrell Nicholas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MatBorder CalculatorTests/MatBorder_CalculatorTests.m
@@ -0,0 +1,40 @@
+//
+//  MatBorder_CalculatorTests.m
+//  MatBorder CalculatorTests
+//
+//  Created by Darrell Nicholas on 1/24/15.
+//  Copyright (c) 2015 Darrell Nicholas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface MatBorder_CalculatorTests : XCTestCase
+
+@end
+
+@implementation MatBorder_CalculatorTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
